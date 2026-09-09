01f5a16d4ac7096dad4a5735a53bad00fa38474c
Prateek Sangwan <mr.prateek.95@gmail.com>
2026-09-09 18:30:55 +0530

    Power of Two - 2026-09-09T13:00:53.389Z

--- 231-power-of-two/solution.cpp
+++ 231-power-of-two/solution.cpp
@@ -2,10 +2,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-    if(n <= 0) return false;
-    for(int i = 0; i < 31; i++) {
-        if((1<<i)==n) return true;
-    }
-    return false;
+    return n > 0 && (n & (n - 1)) == 0;
 }
 };
