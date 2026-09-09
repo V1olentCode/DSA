e0860d992b4456d80579f590aca1394a884895e0
Prateek Sangwan <mr.prateek.95@gmail.com>
2026-09-09 18:27:51 +0530

    Power of Two - 2026-09-09T12:57:49.809Z

--- 231-power-of-two/solution.cpp
+++ 231-power-of-two/solution.cpp
@@ -1,20 +1,11 @@
 // 0 ms | 7.9 MB
 class Solution {
 public:
-    bool power(int n,long long cp){
-        if(cp==n)
-        return true;
-        else if(cp>n || cp<=0)
-        return false;
-        
-        return power(n,cp*2);
-    }
-
-
     bool isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
-        }
-        return power(n,1);
+    if(n <= 0) return false;
+    for(int i = 0; i < 31; i++) {
+        if((1<<i)==n) return true;
     }
+    return false;
+}
 };
