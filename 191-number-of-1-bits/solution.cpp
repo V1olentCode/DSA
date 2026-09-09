8e82556b323ae50fff614518cd2273fc413982ef
Prateek Sangwan <mr.prateek.95@gmail.com>
2026-09-09 18:22:38 +0530

    Number of 1 Bits - 2026-09-09T12:52:36.225Z

--- 191-number-of-1-bits/solution.cpp
+++ 191-number-of-1-bits/solution.cpp
@@ -1,10 +1,11 @@
-// 0 ms | 8.3 MB
+// 0 ms | 8.2 MB
 class Solution {
 public:
     int hammingWeight(int n) {
         int count=0;
-        for(int i = 0; i < 32; i++) {
-            if(n&(1<<i)) count++;
+        while(n>0) {
+            n = n&(n-1);
+            count++;
         }
         return count;
     }
