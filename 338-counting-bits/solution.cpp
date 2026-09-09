05ee9a955e8857520f5df2932c2a259c2bdd7c1f
Prateek Sangwan <mr.prateek.95@gmail.com>
2026-09-09 18:44:30 +0530

    Counting Bits - 2026-09-09T13:14:28.729Z

---
+++ 338-counting-bits/solution.cpp
@@ -0,0 +1,12 @@
+// 0 ms | 10.6 MB
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1);
+        dp[0]=0;
+        for(int i=1;i<=n;i++) {
+            dp[i]=dp[i>>1]+(i&1);
+        }
+        return dp;
+    }
+};
